d095568d2205b7cf68eb9e3de066e6a0f80fcdfb
Jose Emanuel Delgado <emadevjs@gmail.com>
2022-01-07 11:29:57 -0300

    bug fixed on paginator

--- src/components/Paginator/Paginator.js
+++ src/components/Paginator/Paginator.js
@@ -18,6 +18,7 @@ const Paginator = ({
 
   useEffect(() => {
     debouncePageChange(counterSol);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [counterSol]);
 
   const minSol = () => {
