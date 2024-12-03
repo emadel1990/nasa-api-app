bf827ee31e738e8004d93bc231bd0e201e525390
Emanuel Delgado <emadevjs@gmail.com>
2024-12-03 14:50:14 +0000

    Update getNasaManifest.js

--- src/services/getNasaManifest.js
+++ src/services/getNasaManifest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getNasaManifest = async (rover) => {
-  const url = `https://api.nasa.gov/mars-photos/api/v1/${rover}/?api_key=${process.env.REACT_APP_NASA_API}`;
+  const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/?api_key=${process.env.REACT_APP_NASA_API}`;
   const res = await axios.get(url);
   return res.data;
 };
